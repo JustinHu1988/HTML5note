fed7421f8f6deb19e9188291424ea8f34aec681e
Justin Hu <JustinHu1988@outlook.com>
2016-09-01 09:25:39 +0800

    add

--- 001/006.js
+++ 001/006.js
@@ -5,6 +5,8 @@ var pages = document.getElementById("pages"),
 	pageHeight = $(pages).height(); //直接使用原生.style.height获取不到height值
 
 pages.addEventListener("touchstart", function(e){
+	e.preventDefault();
+	e.stopPropagation();
 	//获取初始touch坐标
 	touchStartPostion.x = e.changedTouches[0].clientX;
 	touchStartPostion.y = e.changedTouches[0].clientY;
@@ -14,6 +16,7 @@ pages.addEventListener("touchstart", function(e){
 pages.addEventListener("touchmove", function(e){
 	//纵向跟手移动
 	e.preventDefault();
+	e.stopPropagation();
 	nowClass.css('transform','translateY('+ (e.changedTouches[0].clientY-touchStartPostion.y) + 'px)');
 });
 
@@ -30,6 +33,7 @@ var movevh = function(){
 pages.addEventListener("touchend",function(e){
 	console.log(e);
 	e.preventDefault();
+	e.stopPropagation();
 	moveYDestance = e.changedTouches[0].clientY - touchStartPostion.y;
 	if(moveYDestance>-50){
 		movevh();
